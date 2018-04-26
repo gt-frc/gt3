0072b6fe191d515158b41be8c66be7ea98c0af6b
rogue26 <maxwell.d.hill@gmail.com>
2018-04-26 10:47:51 -0400

    added recombination cross section to background.

--- background.py
+++ background.py
@@ -677,9 +677,48 @@ class background():
                                        (Ti_mod,Tn_mod), 
                                        method='linear', rescale=False)
 
+        def calc_svrec_st(): 
+            #TODO: check this calculation. -MH
+            znint = np.array([16,18,20,21,22])
+            Tint  = np.array([-1,0,1,2,3])
+        
+            rec = np.array([[-1.7523E+01, -1.6745E+01, -1.5155E+01, -1.4222E+01, -1.3301E+01],
+                            [-1.8409E+01, -1.8398E+01, -1.8398E+01, -1.7886E+01, -1.7000E+01],
+                            [-1.9398E+01, -1.9398E+01, -1.9398E+01, -1.9398E+01, -1.9398E+01],
+                            [-2.0155E+01, -2.0155E+01, -2.0155E+01, -2.0155E+01, -2.0155E+01],
+                            [-2.1000E+01, -2.1000E+01, -2.1000E+01, -2.1000E+01, -2.1000E+01]])
+            
+            interp1 = interp2d(znint,Tint,rec)
+            
+            zni_exps   = np.linspace(16,22,100)
+            Ti_exps    = np.linspace(-1, 3,100)
+            svrec_vals = 10.0**(interp1(zni_exps,Ti_exps)) #in m^3/s
+            
+            zni_vals   = np.logspace(16,22,100)
+            Ti_vals    = np.logspace(-1, 3,100)*1.6021E-19 #in joules
+            
+            dsvrec_dTi_vals  = np.gradient(svrec_vals,Ti_vals,axis=0)
+                
+            zni_vals2d, Ti_vals2d = np.meshgrid(zni_vals,Ti_vals)
+            
+            zni_mod = np.where(self.ni>1E22, 1E22, self.ni)
+            zni_mod = np.where(self.ni<1E16, 1E16, zni_mod)
+            Ti_mod  = np.where(self.Ti_ev>1E3,   1E3 * 1.6021E-19, self.Ti_ev*1.6021E-19)
+            Ti_mod  = np.where(self.Ti_ev<1E-1, 1E-1 * 1.6021E-19, Ti_mod)
+                
+            self.svrec     = griddata(np.column_stack((zni_vals2d.flatten(), Ti_vals2d.flatten())), 
+                                       svrec_vals.flatten(), 
+                                       (zni_mod,Ti_mod),
+                                       method='linear', rescale=False)
+            
+            self.dsvrec_dT = griddata(np.column_stack((zni_vals2d.flatten(), Ti_vals2d.flatten())), 
+                                       dsvrec_dTi_vals.flatten(), 
+                                       (zni_mod,Ti_mod), 
+                                       method='linear', rescale=False)
 
         calc_sigv_fus()
         calc_sigv_ion()
         calc_svel()
         calc_svcxi_st()
+        calc_svrec_st()
         
