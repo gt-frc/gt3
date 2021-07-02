75e1299d5f829dc56b2d7cd5b5851feeb2dbf6a8
Jonathan R <veto1024@gmail.com>
2021-07-02 13:38:51 -0400

    Correctly check neutpy version
    Signed-off-by: Jonathan R <veto1024@gmail.com>

--- GT3/Neutrals/__init__.py
+++ GT3/Neutrals/__init__.py
@@ -7,7 +7,8 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError("Neutpy is not installed or could not be loaded. Neutrals data will be unavailable.")
 try:
-    if version.parse(pkg_resources.get_distribution('numpy').version) < version.parse('0.0.5'):
+    import neutpy
+    if version.parse(neutpy.__version__) < version.parse('0.0.5'):
         raise ImportError("NeutPy v0.0.5 or newer required")
 except pkg_resources.DistributionNotFound as e:
     raise
